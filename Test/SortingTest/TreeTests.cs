19a5b43ba688065eef775fc63190bd39baa4d8f9
Ankita Nandy <nandy.ankita@gmail.com>
2022-09-30 01:50:55 +0530

    Tree Traversal added

--- Test/SortingTest/TreeTests.cs
+++ Test/SortingTest/TreeTests.cs
@@ -1,12 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Text;
-using Sorting;
 using TreeAlgos;
-public class Program{
+namespace SortingTest;
 
-    public static void Main(){
- var n1= new BinaryTree(1);
+[TestClass]
+public class TreeTests
+{
+    [TestMethod]
+    public void TestInorderTraversal()
+    {
+        var n1= new BinaryTree(1);
         var n2= new BinaryTree(2);
         var n3= new BinaryTree(3);
         var n4= new BinaryTree(4);
@@ -23,12 +24,12 @@ public class Program{
         n3.right=n7;
        n4.left=n8;
 
-    //   var inorderExpected = new int[]{8,4,2,5,1,6,3,7};
+       var inorderExpected = new int[]{8,4,2,5,1,6,3,7};
        var actual = n1.InorderTraversal();
-       /*for(int i=0;i<inorderExpected.Length;i++)
+       for(int i=0;i<inorderExpected.Length;i++)
        {
-             Assert.AreEqual(actual[i],expected[i]);
-       } */    
-Console.WriteLine(string.Join(",",actual));
+             Assert.AreEqual(actual[i],inorderExpected[i]);
+       }     
     }
 }
+ 
---
+++ TreeAlgos/BinaryTree.cs
@@ -0,0 +1,32 @@
+﻿namespace TreeAlgos;
+public class BinaryTree
+{
+    public int value;
+    public BinaryTree left;
+    public BinaryTree right;
+
+    public BinaryTree(int val=0, BinaryTree left=null, BinaryTree right=null)
+    {
+        value=val;
+        this.left=left;
+        this.right=right;
+    }
+
+    public List<int> InorderTraversal()
+    {
+        BinaryTree root=this;
+        List<int> result=new List<int>();
+        return InorderTraversal(root,result);
+    }
+    public List<int> InorderTraversal(BinaryTree node,List<int> result)
+    {
+            if(node!=null)
+            {
+                InorderTraversal(node.left,result);
+                result.Add(node.value);
+                InorderTraversal(node.right,result);
+            }
+
+            return result;
+    }
+}
